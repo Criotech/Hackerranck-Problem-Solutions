fbab9169f1af98553f053cba32e33b770b4f0c5f
Opeoluwa <opesiyanbola8991@gmail.com>
2021-04-05 01:37:52 +0100

    Sales By Match

---
+++ salesByMatch.js
@@ -0,0 +1,14 @@
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    let hash = {};
+    let noPairs = 0;
+    
+    for (let i=0; i<ar.length; i++) {
+        hash[ar[i]] ? hash[ar[i]]+=1 : hash[ar[i]]=1;
+        
+        if (hash[ar[i]]%2 === 0) {
+            noPairs++
+        }
+    }
+    return noPairs
+}
